56822c7bce017d057af6432db161004ba4f9cce4
fufangyuan <zyfcffy@163.com>
2020-11-07 22:08:33 +0800

    [fufangyuan]feat:use proxyMode

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,10 +1,14 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_SINGLETON;
+
 @RestController
+@Scope(SCOPE_SINGLETON)
 public class GreetingController {
 
     private final GreetingService greetingService;
--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,9 +1,17 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 
 @Service
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE, proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class GreetingService {
+
+    public GreetingService(){
+        System.out.println("init service");
+    }
 
     String sayHi() {
         return "hello world";
